99ce43e31ea560f94b9949f96a1d504e69edc319
unknown <zcfshr@gmail.com>
2022-09-26 13:17:30 -0600

    fixed layout of cards, started readme file

--- src/template.js
+++ src/template.js
@@ -83,7 +83,7 @@ function createTeam(data) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Team Profile Generator</title>
     
     <link
       rel="stylesheet"
@@ -94,9 +94,11 @@ function createTeam(data) {
 </head>
 <body>
     <header>
-        <h1>My Team</h1>
+        <h1 class="header">My Team</h1>
     </header>
+    <div class="row">
         ${employees(data)}
+        </div>
     
     
 </body>
